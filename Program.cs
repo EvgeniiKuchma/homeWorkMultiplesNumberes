607e42a2e48d8ea89a2a176ca31c2d92c58774d4
Евгений Кучма <desferto87@gmail.com>
2024-12-23 00:11:10 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace homeWorkMultiplesNumberes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberN;
+            int minValue = 10;
+            int maxValue = 26;
+            int startNumberRange = 50;
+            int endNumberRange = 151;
+            int countMultiplesNumber = 0;
+
+            Random random = new Random();
+            numberN = random.Next(minValue, maxValue);
+
+            Console.WriteLine(numberN);
+            Console.WriteLine("----------------------");
+
+            for (int i = numberN; i < endNumberRange; i += numberN)
+            {
+                if (i >= startNumberRange)
+                {
+                    countMultiplesNumber++;
+                }
+            }
+
+            Console.WriteLine(countMultiplesNumber);
+        }
+    }
+}
